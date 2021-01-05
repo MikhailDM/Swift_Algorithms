d70745ba9a7b758690b6bb224dac0649555f7612
Mikhail Dmitriev <misha.dmitriev.spb@gmail.com>
2021-01-05 18:06:16 +0300

    007. Slow again

--- Problem 007.playground/Contents.swift
+++ Problem 007.playground/Contents.swift
@@ -9,27 +9,62 @@ import Foundation
 class Solution {
     
     var max: Int
+    var primes = [Int]()
     
     init(max: Int) {
         self.max = max
     }
     
+    /*
+     N = 10001
+     def divisible(n,plist):
+         for p in plist:
+             if n%p == 0:
+                 return True
+             else:
+                 pass
+         return False
+     P = [2]
+     for i in range(1,N):
+         s = P[i-1] + 1
+         while s:
+             if divisible(s,P):
+                 s += 1
+                 continue
+             else:
+                 break
+         P += [s]
+     print(P[-1])
+     */
     func isNatural(num: Int) -> Bool {
-        guard num >= 2 else { return false }
+        guard num >= 3 else { return false }
         guard num != 2 else { return true }
-        guard num % 2 != 0 else { return false }
-        return !stride(from: 3, through: Int(sqrt(Double(num))), by: 2).contains { num % $0 == 0 }
+        for y in stride(from: 3, through: Int(sqrt(Double(num))), by: 2) {
+            if num % y == 0 { return false }
+        }
+        return true
+//        prime = True
+//        for i in range(2, int(math.sqrt(n)+1)):
+//            if n % i == 0:
+//                prime = False
+//        if prime:
+//            prime_list.append(n)
+        
+//        guard num >= 2 else { return false }
+//        guard num != 2 else { return true }
+//        guard num % 2 != 0 else { return false }
+//        return !stride(from: 3, through: Int(sqrt(Double(num))), by: 2).contains { num % $0 == 0 }
     }
     
     func getResult() -> Int {
-        var naturals = [2]
-        var i = 3
-        while naturals.count < max {
-            if isNatural(num: i) { naturals.append(i) }
+        var i = 2
+        while primes.count < max {
+            guard i != 2 else { primes.append(i); i += 1; continue }
+            if isNatural(num: i) { primes.append(i) }
             i += 2
         }
-        print("===== List: \(naturals)")
-        return naturals.last ?? 0
+        print("===== List: \(primes)")
+        return primes.last ?? 0
     }
     
 }//
@@ -42,6 +77,28 @@ print("")
 let solution1001 = Solution(max: 10001)
 print("===== Result: \(solution1001.getResult())")
 
+/*
+ # This function is called repeatedly by the generate_num() function
+ def check_prime(n):
+     prime = True
+     for i in range(2, int(math.sqrt(n)+1)):
+         if n % i == 0:
+             prime = False
+     if prime:
+         prime_list.append(n)
+
+
+ def generate_num():
+     i = 2
+     while True:
+         check_prime(i)
+         if len(prime_list) >= limit:
+             return prime_list[-1]
+         i += 1
+
+
+ print(generate_num()) # Output is: 104743
+ */
 
 
 
